e49be3a79439ebb01e5bf7adbfeaad19f4eaa4e6
ikeda-edash <t.ikeda@e-dash.io>
2024-10-31 19:44:49 +0900

    reverse list

---
+++ slack/consts/consts.go
@@ -0,0 +1,6 @@
+package consts
+
+const (
+	FROM_SLACK_MESSAGE = "output.md"
+	FOR_RAG_MESSAGE    = "output2.md"
+)
--- slack/main.go
+++ slack/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"log"
 	"os"
+	consts "slack-bot/consts"
 	"strings"
 	"time"
 
@@ -10,28 +12,33 @@ import (
 )
 
 func main() {
-	sc := slack.New(os.Getenv("MAPLE_BOT_TOKEN"))
+	slackClient := slack.New(os.Getenv("MAPLE_BOT_TOKEN"))
 	channelID := os.Getenv("MAPLE_CHANNEL_ID")
 
-	err := getMessages(sc, channelID)
+	err := getMessages(slackClient, channelID)
+	if err != nil {
+		panic(err)
+	}
+
+	// outpt.mdの順番を逆にする
+	err = reverseFile(consts.FROM_SLACK_MESSAGE, consts.FOR_RAG_MESSAGE)
 	if err != nil {
 		panic(err)
 	}
 }
 
-func getMessages(sc *slack.Client, channelID string) error {
+func getMessages(slackClient *slack.Client, channelID string) error {
 	hasMore := true
 	cursor := ""
 
-	file := "output.md"
-	f, err := os.Create(file)
+	f, err := os.Create(consts.FROM_SLACK_MESSAGE)
 	if err != nil {
 		return err
 	}
 	defer f.Close()
 
 	for hasMore {
-		h, err := getHistories(sc, channelID, cursor)
+		h, err := getHistories(slackClient, channelID, cursor)
 		if err != nil {
 			return err
 		}
@@ -42,7 +49,7 @@ func getMessages(sc *slack.Client, channelID string) error {
 		hasMore = h.HasMore
 
 		for _, m := range h.Messages {
-			ms, err := getConversations(sc, channelID, m.Timestamp)
+			ms, err := getConversations(slackClient, channelID, m.Timestamp)
 			if err != nil {
 				return err
 			}
@@ -54,21 +61,20 @@ func getMessages(sc *slack.Client, channelID string) error {
 				return err
 			}
 		}
-		break
 	}
 
 	return nil
 }
 
-func getHistories(sc *slack.Client, channelID, cursor string) (*slack.GetConversationHistoryResponse, error) {
-	return sc.GetConversationHistory(&slack.GetConversationHistoryParameters{
+func getHistories(slackClient *slack.Client, channelID, cursor string) (*slack.GetConversationHistoryResponse, error) {
+	return slackClient.GetConversationHistory(&slack.GetConversationHistoryParameters{
 		ChannelID: channelID,
 		Cursor:    cursor,
 	})
 }
 
-func getConversations(sc *slack.Client, channelID, ts string) ([]slack.Message, error) {
-	ms, _, _, err := sc.GetConversationReplies(&slack.GetConversationRepliesParameters{
+func getConversations(slackClient *slack.Client, channelID, ts string) ([]slack.Message, error) {
+	ms, _, _, err := slackClient.GetConversationReplies(&slack.GetConversationRepliesParameters{
 		ChannelID: channelID,
 		Timestamp: ts,
 	})
@@ -107,6 +113,69 @@ func writeMessages(f *os.File, ms []slack.Message, channelID string) error {
 	if err != nil {
 		return err
 	}
+
+	return nil
+}
+
+func reverseFile(inputFile, outputFile string) error {
+	// 元ファイルを開く
+	f, err := os.Open(inputFile)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	// 出力ファイルを作成
+	out, err := os.Create(outputFile)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// 元ファイルのサイズを取得
+	stat, err := f.Stat()
+	if err != nil {
+		return err
+	}
+	fileSize := stat.Size()
+
+	var line []byte
+	buf := make([]byte, 1)
+
+	writer := bufio.NewWriter(out)
+
+	// ファイル末尾から逆順に読み込み
+	for offset := fileSize - 1; offset >= 0; offset-- {
+		_, err := f.Seek(offset, 0)
+		if err != nil {
+			return err
+		}
+
+		_, err = f.Read(buf)
+		if err != nil {
+			return err
+		}
+
+		if buf[0] == '\n' && len(line) > 0 {
+			// 行を逆順にして保存
+			for i := len(line) - 1; i >= 0; i-- {
+				writer.WriteByte(line[i])
+			}
+			writer.WriteByte('\n')
+			line = nil
+		} else {
+			line = append(line, buf[0])
+		}
+	}
+
+	// 最後の行を逆順にして保存
+	for i := len(line) - 1; i >= 0; i-- {
+		writer.WriteByte(line[i])
+	}
+	writer.WriteByte('\n')
+
+	// 書き込みを確定
+	writer.Flush()
 
 	return nil
 }
